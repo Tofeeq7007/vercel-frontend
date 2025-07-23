8c38cb364782207dd77ce3b0e4409831273f76bc
Tofeeq7007 <tofeeqraja9100@gmail.com>
2025-07-23 14:28:12 +0530

    Responsive2

--- src/component/shortendlink.jsx
+++ src/component/shortendlink.jsx
@@ -9,27 +9,27 @@ export function ShortendLink({shorturl}){
         setTimeout(()=>setCopied(false),2000);
     }
     return (
-        <>
+        <div className="flex flex-col">
         
             <div className="mt-4 font-semibold">Your Short URL:</div>
             <div className="bg-green-100
-            mt-4 p-3 flex justify-between">
+            mt-4 p-3  flex justify-between">
                 <a
                     href={shorturl}
                     target="_blank"
-                    className="text-blue-600 underline"
+                    className="text-blue-600 max-[375px]:overflow-hidden underline"
                 >
                     {shorturl}
                 </a>
                 <div>
                 {!copied && (
-                    <MdContentCopy className="cursor-pointer text-2xl" onClick={handleCopy}/>
+                    <MdContentCopy className="cursor-pointer max-sm:hidden text-2xl" onClick={handleCopy}/>
                 )}
                 {copied && (
                     <span className="text-green-600 text-xs mt-1 animate-bounce">Copied!</span>
                 )}
                 </div>
             </div>
-        </>
+        </div>
     )
 }
